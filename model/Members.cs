9d67c2983ff9585115ba05b86d208d0466413417
antonstrand <jag@antonstrand.se>
2018-09-27 20:02:48 +0200

    Init Members

---
+++ model/Members.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace _1dv607_w2.model
+{
+  class Members
+  {
+    private FileSystem _fileSystem;
+    private List<Member> _members;
+    public Members(FileSystem fileSystem)
+    {
+      _fileSystem = fileSystem;
+    }
+  }
+}
