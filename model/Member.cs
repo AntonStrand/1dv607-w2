a5715c69e1d1563fa932f2875f15233ca4695c90
antonstrand <jag@antonstrand.se>
2018-09-27 20:02:10 +0200

    Init Member

---
+++ model/Member.cs
@@ -0,0 +1,15 @@
+namespace _1dv607_w2.model
+{
+  class Member
+  {
+    private string _name;
+    private string _ssn;
+    private int _id;
+    public Member(string name, string ssn, int id)
+    {
+      _name = name;
+      _ssn = ssn;
+      _id = id;
+    }
+  }
+}
