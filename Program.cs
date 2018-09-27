4435fa63629891e33d0b9c0aeffe5849ffb3bc96
antonstrand <jag@antonstrand.se>
2018-09-27 20:03:38 +0200

    Init the application

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
-
-namespace _1dv607_w2
+﻿namespace _1dv607_w2
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      model.FileSystem fs = new model.FileSystem("members.json");
+      model.Members m = new model.Members(fs);
+      view.ConsoleGUI v = new view.ConsoleGUI();
+      controller.Administrator c = new controller.Administrator();
+      while (c.run(v, m)) ;
     }
+  }
 }
