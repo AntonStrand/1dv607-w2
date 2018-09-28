7cbb751634ce389f5ada5969cd171a5727c87f9d
antonstrand <jag@antonstrand.se>
2018-09-28 17:06:32 +0200

    Parse JSON to List<Member>

--- model/FileSystem.cs
+++ model/FileSystem.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
 namespace _1dv607_w2.model
 {
   class FileSystem
@@ -9,9 +14,29 @@ namespace _1dv607_w2.model
       _path = path;
     }
 
-    public void save(string txt)
+    public void Save(string txt)
     {
-      System.IO.File.WriteAllText(_path, txt);
+      File.WriteAllText(_path, txt);
+    }
+
+    public List<T> GetParsedJSON<T>()
+    {
+      try
+      {
+        string unparsedJSON = File.ReadAllText(_path);
+        try
+        {
+          return (List<T>)JsonConvert.DeserializeObject<IEnumerable<T>>(unparsedJSON);
+        }
+        catch (Exception)
+        {
+          throw new ArgumentException($"The JSON could not be converted to {typeof(T)}.");
+        }
+      }
+      catch (FileNotFoundException)
+      {
+        throw new FileNotFoundException($"The file, \"{_path}\", could not be found.");
+      }
     }
   }
 }
