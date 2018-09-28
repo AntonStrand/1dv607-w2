748800734c64c6dc88af14e7705be7d958950c3b
antonstrand <jag@antonstrand.se>
2018-09-28 17:16:25 +0200

    Importing all members

--- model/Members.cs
+++ model/Members.cs
@@ -6,9 +6,17 @@ namespace _1dv607_w2.model
   {
     private FileSystem _fileSystem;
     private List<Member> _members;
+
     public Members(FileSystem fileSystem)
     {
       _fileSystem = fileSystem;
+      GetMembers();
+      foreach (Member m in _members)
+      {
+        System.Console.WriteLine(m.ToString());
+      }
     }
+
+    private void GetMembers() => _members = _fileSystem.GetParsedJSON<Member>();
   }
 }
