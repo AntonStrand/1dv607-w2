6b383a7739dff3622465fb788d1cb7244bb349c4
antonstrand <jag@antonstrand.se>
2018-10-01 19:06:58 +0200

    Setting for publishing the app

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
 
 namespace _1dv607_w2
 {
@@ -6,11 +8,14 @@ namespace _1dv607_w2
   {
     static void Main(string[] args)
     {
-      model.FileSystem fs = new model.FileSystem("members.json");
+      // TODO: use this when exporting
+      // string pathToJson = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/members.json";
+      string pathToJson = "members.json";
+      model.FileSystem fs = new model.FileSystem(pathToJson);
       model.Members m = new model.Members(fs);
       view.ConsoleGUI v = new view.ConsoleGUI();
       controller.Administrator c = new controller.Administrator(v, m);
-      while ( c.run() );
+      while (c.run()) ;
     }
   }
 }
