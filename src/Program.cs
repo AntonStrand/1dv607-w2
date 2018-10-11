ea5bdb176086ff2f52bd76fbe5240d3bcfaf4d64
antonstrand <jag@antonstrand.se>
2018-10-11 16:06:49 +0200

    Try catch the whole application

--- src/Program.cs
+++ src/Program.cs
@@ -8,12 +8,22 @@ namespace _1dv607_w2
   {
     static void Main(string[] args)
     {
-      string pathToJson = "members.json";
-      model.FileSystem fs = new model.FileSystem(pathToJson);
-      model.Members m = new model.Members(fs);
-      view.console.ConsoleUI v = new view.console.ConsoleUI();
-      controller.Administrator c = new controller.Administrator(v, m);
-      while (c.run()) ;
+      try
+      {
+        string pathToJson = "members.json";
+        model.FileSystem fs = new model.FileSystem(pathToJson);
+        model.Members m = new model.Members(fs);
+        view.console.ConsoleUI v = new view.console.ConsoleUI();
+        controller.Administrator c = new controller.Administrator(v, m);
+        while (c.run()) ;
+      }
+      catch (System.Exception)
+      {
+        System.Console.Clear();
+        System.Console.WriteLine("Sorry, something unexpected happened.");
+        System.Console.WriteLine("Please restart the application.");
+        System.Console.ReadKey();
+      }
     }
   }
 }
